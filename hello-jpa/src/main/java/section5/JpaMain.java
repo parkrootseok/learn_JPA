15caa3de103bcccbb68869e65c035cc1eed72400
ParkRootSeok <g_e_un@naver.com>
2022-10-27 20:30:35 +0900

    단방향 -> 양방향 관계로 수정 및 주인 등록

--- hello-jpa/src/main/java/section5/JpaMain.java
+++ hello-jpa/src/main/java/section5/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -28,6 +29,22 @@ public class JpaMain {
             member.setName("member1");
             member.setTeam(team); // Team 객체를 저장해야 할 것 같은데...
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            /* 양방향 연관 관게를 이용해 찾은 멤버가 속한 팀의 멤버 구하기 */
+            Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m : members) {
+                System.out.println("m = " + m.getName());
+            }
+
+            /* 양방향 연관 관계를 이용할 때 반드시 주인 객체에서 수정을 해야한다 */
+            /* 주인 객체는 반드시 양방향 관계를 만들기위해 만든 단방햔 관계가아닌 기존의 단방향 관계를 지닌 클래스이다 */
+            // team.getMembers().add(member); 잘못된 데이터 수정 - Team 객체를 관리하는 것은 Member 클래스
+            member.setTeam(team); // 옳은 방법 - 주인인 Member 클래스에서 데이터 수정이 이루어져야 한다.
 
             tx.commit();
         } catch (Exception e) {
--- hello-jpa/src/main/java/section5/Team.java
+++ hello-jpa/src/main/java/section5/Team.java
@@ -1,18 +1,21 @@
 package section5;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
 
     @Id @GeneratedValue
+    @Column(name = "TEAM_ID")
     private Long id;
 
     @Column(name = "USERNAME")
     private String name;
+
+    @OneToMany(mappedBy = "team") // 1:N(Team:Member), mappedby = "team"은 Member 클래스의 team에 의해 관리됨을 의미.
+    private List<Member> members = new ArrayList<>();
 
     public Team() {}
 
@@ -30,5 +33,13 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
