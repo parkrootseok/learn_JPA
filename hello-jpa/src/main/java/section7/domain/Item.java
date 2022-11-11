4c1e5d2ee72894d3026700cc48a2e046c0e76dca
ParkRootSeok <g_e_un@naver.com>
2022-11-11 15:47:53 +0900

    feat(section7) : 부모 테이블에 상속 받은 클래스에 대한 정보를 입력해주는 어노테이션 추가

--- hello-jpa/src/main/java/section7/domain/Item.java
+++ hello-jpa/src/main/java/section7/domain/Item.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED) // 조인 전략
+@DiscriminatorColumn(name = "DTYPE") // 부모 테이블에 상속받은 클래스에 대한 정보 추가
 public class Item {
 
     @Id @GeneratedValue
