fa40a2635a17f0bf861b4ef6bdd85ac50cd36ccc
ParkRootSeok <g_e_un@naver.com>
2022-11-11 15:49:54 +0900

    feat(section7) : 부모 테이블에 정보로 표현될 값을 변경하는 어노테이션 추가

--- hello-jpa/src/main/java/section7/domain/Album.java
+++ hello-jpa/src/main/java/section7/domain/Album.java
@@ -1,5 +1,10 @@
 package section7.domain;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("A")
 public class Album extends Item {
 
     private String artist;
--- hello-jpa/src/main/java/section7/domain/Movie.java
+++ hello-jpa/src/main/java/section7/domain/Movie.java
@@ -2,9 +2,11 @@ package section7.domain;
 
 import net.bytebuddy.matcher.InstanceTypeMatcher;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("M")
 public class Movie extends Item {
 
     private String director;
