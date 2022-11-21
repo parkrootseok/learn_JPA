6b04997a349c36a65434bf3de94aa5c800717409
ParkRootSeok <g_e_un@naver.com>
2022-11-21 22:59:12 +0900

    feat(section8) : orphanRemoval 적용

--- hello-jpa/src/main/java/JpaMain.java
+++ hello-jpa/src/main/java/JpaMain.java
@@ -30,8 +30,12 @@ public class JpaMain {
             parent.addChild(child2);
 
             em.persist(parent);
-            // em.persist(child1);
-            // em.persist(child2);
+
+            em.flush();
+            em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildren().remove(0);
 
             tx.commit();
 
