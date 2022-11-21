3eda6b0b0f104ce8a98542e56078dcb96b9af0cf
ParkRootSeok <g_e_un@naver.com>
2022-11-21 23:12:41 +0900

    feat(section8) : 영속성 전이 설정

--- hello-jpa/src/main/java/section8/domain/Orders.java
+++ hello-jpa/src/main/java/section8/domain/Orders.java
@@ -17,10 +17,10 @@ public class Orders extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY) @JoinColumn(name = "delivery_id")
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL) @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private String orderDate;
