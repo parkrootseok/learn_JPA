0a9683620cce5280a70cf524d3838ca02bc6ea54
ParkRootSeok <g_e_un@naver.com>
2023-01-03 02:08:10 +0900

    OrderService, OrderRepository 구현

---
+++ JPA 활용 1/jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,62 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.config.status.DeliveryStatus;
+import jpabook.jpashop.domain.delivery.Delivery;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.member.Member;
+import jpabook.jpashop.domain.order.Order;
+import jpabook.jpashop.domain.orderitem.OrderItem;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderService {
+
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final ItemRepository itemRepository;
+
+    /**
+     * 주문
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        Member member = memberRepository.find(memberId);
+        Item item = itemRepository.find(itemId);
+
+        // 배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.READY);
+
+        // 주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        Order order = orderRepository.findOrder(orderId);
+
+        order.cancel();
+    }
+
+
+
+}
