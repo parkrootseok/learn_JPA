e863dae6e406b2a39f528948037a51dd500dcea2
ParkRootSeok <g_e_un@naver.com>
2023-01-03 01:25:40 +0900

    feat : Stock 조절 비즈니스 로직 추가

--- JPA 활용 1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ JPA 활용 1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.domain.item;
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.CategoryItem;
 import jpabook.jpashop.domain.category.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -33,5 +34,24 @@ public class Item {
     @OneToMany(mappedBy = "item")
     private List<CategoryItem> categoryItems = new ArrayList<>();
 
+    /**
+     * Stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * Stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+
+        if (restStock < 0) {
+            throw new NotEnoughStockException("재고가 없습니다.");
+        }
+
+        this.stockQuantity = restStock;
+    }
 
 }
