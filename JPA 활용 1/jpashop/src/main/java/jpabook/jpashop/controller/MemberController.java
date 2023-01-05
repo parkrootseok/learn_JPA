f03ba59869bc63cb09582a6b601f4304844fa8cf
ParkRootSeok <g_e_un@naver.com>
2023-01-06 01:08:02 +0900

    회원 목록 조회 기능 구현

--- JPA 활용 1/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ JPA 활용 1/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -43,4 +43,11 @@ public class MemberController {
         return "redirect:/";
     }
 
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "/members/memberList";
+    }
+
 }
