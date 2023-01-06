90847f199bdbfd207c62fa9002068cdc59c67696
ParkRootSeok <g_e_un@naver.com>
2023-01-07 02:36:15 +0900

    회원 조회 API Version1

--- JPA 활용/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ JPA 활용/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -12,6 +12,8 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController // @Controller + @ResponseBody 를 합친 어노테이션
 @RequiredArgsConstructor
 public class MemberApiController {
@@ -66,5 +68,24 @@ public class MemberApiController {
 
         return new PutMemberRes(findMember.getId(), findMember.getName());
     }
+
+    /**
+     * 조회 V1: 응답 값으로 엔티티를 직접 외부에 노출한다.
+     * 문제점
+     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     * - 기본적으로 엔티티의 모든 값이 노출된다.
+     * - 응답 스펙을 맞추기 위해 로직이 추가된다. (@JsonIgnore, 별도의 뷰 로직 등등)
+     * - 실무에서는 같은 엔티티에 대해 API가 용도에 따라 다양하게 만들어지는데, 한 엔티티에 각각의
+     API를 위한 프레젠테이션 응답 로직을 담기는 어렵다.
+     * - 엔티티가 변경되면 API 스펙이 변한다.
+     * - 추가로 컬렉션을 직접 반환하면 항후 API 스펙을 변경하기 어렵다.(별도의 Result 클래스
+     생성으로 해결)
+     * 결론
+     * - API 응답 스펙에 맞추어 별도의 DTO를 반환한다.
+     */
+    public List<Member> getMembersV1() {
+        return memberService.findMembers();
+    }
+    //조회 V1: 안 좋은 버전, 모든 엔티티가 노출, @JsonIgnore -> 이건 정말 최악, api가 이거 하나인가! 화면에 종속적이지 마라!
 
 }
